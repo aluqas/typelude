7946156956a86bb3c63472717705bf6590e3d4a3
saqula <ping@saqu.la>
2025-12-18 10:28:38 +0900

    refactor(prelude): update re-exports for new conventions
    - Re-export types from kernel module
    - Update std module re-exports

--- src/prelude.rs
+++ src/prelude.rs
@@ -9,26 +9,103 @@
 
 pub use crate::{
     // Evaluation infrastructure
-    eval::{EApply, EApply2, EIf, ELit, EWhile, Eval, Evaluate},
+    eval::{EApp, EApp2, EIf, ELit, Eval, Evaluate},
+    // Kernel Types & Traits
+    kernel::{
+        array::{Cons, TyArray, TyNil},
+        bool::{TyBool, TyFalse, TyTrue},
+        traits::Apply,
+    },
     // Standard Library
     std::{
         // Array (Collection)
         array::{
-            Cons, EAppend, EConcat, EContains, EGet, EHead, EIsEmpty, ELen, EPrepend, ESet, ETail,
-            FAppend, FConcat, FContains, FGet, FHead, FIsEmpty, FLen, FPrepend, FSet, FTail,
-            TyArray, TyNil,
+            // Exprs
+            EAppend,
+            EConcat,
+            EContains,
+            EFilter,
+            EFold,
+            EGet,
+            EHead,
+            EIsEmpty,
+            ELen,
+            EMap,
+            EPrepend,
+            ESet,
+            ETail,
+            // Ops
+            OpAppend,
+            OpConcat,
+            OpContains,
+            OpFilter,
+            OpFold,
+            OpGet,
+            OpHead,
+            OpIsEmpty,
+            OpLen,
+            OpMap,
+            OpPrepend,
+            OpSet,
+            OpTail,
         },
         // Bool (Logical)
         bool::{
-            Assert, EAnd, ENand, ENor, ENot, EOr, EXnor, EXor, FAnd, FNand, FNor, FNot, FOr,
-            FXnor, FXor, KindBool, ToTyBoolOut, TyFalse, TyTrue,
+            // Exprs
+            Assert,
+            EAnd,
+            ENand,
+            ENor,
+            ENot,
+            EOr,
+            EXnor,
+            EXor,
+            // Traits
+            KindBool,
+            // Ops
+            OpAnd,
+            OpNand,
+            OpNor,
+            OpNot,
+            OpOr,
+            OpXnor,
+            OpXor,
+            ToTyBoolOut,
         },
         // Compare
-        cmp::{EEq, EGe, EGt, ELe, ELt, ENotEq, FEq, FGe, FGt, FLe, FLt, FNeq},
+        cmp::{
+            // Exprs
+            EEq,
+            EGe,
+            EGt,
+            ELe,
+            ELt,
+            ENeq,
+            // Ops
+            OpEq,
+            OpGe,
+            OpGt,
+            OpLe,
+            OpLt,
+            OpNeq,
+        },
         // Int (Arithmetic)
-        int::{EAdd, EDiv, EMul, EPow, ERem, ESub, FAdd, FDiv, FMul, FPow, FRem, FSub},
-        // Traits (Core)
-        traits::TyFn,
+        int::{
+            // Exprs
+            EAdd,
+            EDiv,
+            EMul,
+            EPow,
+            ERem,
+            ESub,
+            // Ops
+            OpAdd,
+            OpDiv,
+            OpMul,
+            OpPow,
+            OpRem,
+            OpSub,
+        },
     },
     // Macros
     tyarray,
